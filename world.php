5f1694d4bcebe29da12e0db63a2f8fcf23356a81
NiaPayton <shadeanpayton@gmail.com>
2022-11-11 19:53:38 -0500

    Added Completed Exercise 3 with CSS and IMG

--- world.php
+++ world.php
@@ -5,13 +5,33 @@ $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
+if (isset($_GET['country']) || !empty($_GET['country'])){
+  $country = filter_var($_GET['country'], FILTER_SANITIZE_STRING);
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+}
+else{
+  $stmt = $conn->query("SELECT * FROM countries");
+}
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Continent</th>
+    <th>Independence</th>
+    <th>Head of State</th>
+  </tr>
+
+  <?php foreach ($results as $row): ?>
+    <tr>
+      <td><?= $row['name']; ?></td>
+      <td><?= $row['continent']; ?></td>
+      <td><?= $row['independence_year']; ?></td>
+      <td><?= $row['head_of_state']; ?></td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+ 
